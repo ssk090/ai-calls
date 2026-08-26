77f2b37cb04477e34e8f2ab5c113f7b439f165b9
Shivananda Sai <shivananda.sai@altir.co>
2026-08-26 21:44:16 +0530

    day 4: token usage

--- hello_llm.py
+++ hello_llm.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from pyexpat.errors import messages
 from dotenv import load_dotenv
 from groq import Groq
 
@@ -13,20 +14,42 @@ client = Groq(api_key=my_api_key)
 
 model="qwen/qwen3.8-27b"
 
-## System Prompt
-system_message = {
-    "role": "system",
-    "content": "You are a helpful assistant."
-}
+prompt1 = "Hi!"
+prompt2 = "What is the capital of India?"
+prompt3 = "explain the theory of relativity in 1000 words"
 
-user_message = {
-    "role": "user",
-    "content": "what is the capital of India ?" ## ask question here   
-}
+prompts = [prompt1, prompt2, prompt3]
 
-messages = [system_message, user_message]
+for prompt in prompts:
+    system_message = {
+        "role": "system",
+        "content": "You are a helpful assistant."
+    }
 
-response = client.chat.completions.create(model=model,messages=messages, temperature=0.7)
-answer = response.choices[0].message.content
+    user_message = {
+        "role": "user",
+        "content": prompt
+    }
+    
+    messages = [system_message, user_message]
+    response = client.chat.completions.create(model=model,messages=messages, temperature=0.7)
+    usage = response.usage
+    print(f"Prompt: {prompt} --> Completion tokens: {usage.completion_tokens}, Prompt tokens: {usage.prompt_tokens}, Total tokens: {usage.total_tokens}")
 
-print(answer)
+# ## System Prompt
+# system_message = {
+#     "role": "system",
+#     "content": "You are a helpful assistant."
+# }
+
+# user_message = {
+#     "role": "user",
+#     "content": "what is the capital of India ?" ## ask question here   
+# }
+
+# messages = [system_message, user_message]
+
+# response = client.chat.completions.create(model=model,messages=messages, temperature=0.7)
+# answer = response.choices[0].message.content
+
+# print(answer)
